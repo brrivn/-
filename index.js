c748cc3926d5ab5fc35dad42ec85d7137a94637f
brrivn <jealkii@gmail.com>
2024-11-22 04:06:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["يحتسي شاي بالنعنع", "🎮 Playing VALORANT"];
+const statusMessages = ["🍵 يحتسي شاي بالنعنع"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
